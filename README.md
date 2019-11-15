19043ac8069075b86a2d5f6c7b8e21d33c33c46b
David Lesieur <david@davidlesieur.com>
2019-11-14 23:15:51 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# KerkoStart
+
+This is the code from the _Getting started_ section of [Kerko]'s README file.
+
+Have a look at [KerkoApp] for a slightly more complete application example.
+
+[Kerko]: https://github.com/whiskyechobravo/kerko
+[KerkoApp]: https://github.com/whiskyechobravo/kerkoapp
---
+++ hello_kerko.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_babelex import Babel
+from flask_bootstrap import Bootstrap
+
+from kerko import blueprint as kerko_blueprint
+from kerko.composer import Composer
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'  # Replace this value.
+app.config['KERKO_ZOTERO_API_KEY'] = 'xxxxxxxxxxxxxxxxxxxxxxxx'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_ID'] = '9999999'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'group'  # Replace this value if necessary.
+app.config['KERKO_COMPOSER'] = Composer()
+
+babel = Babel(app)
+bootstrap = Bootstrap(app)
+
+app.register_blueprint(kerko_blueprint, url_prefix='/bibliography')
