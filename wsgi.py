1cfeeb65f586a4b614efeabd79b34763f5b355cc
David Lesieur <david@davidlesieur.com>
2023-11-06 21:34:13 -0500

    Reformat code with Ruff.

--- wsgi.py
+++ wsgi.py
@@ -11,20 +11,20 @@ app = Flask(__name__)
 config_update(app.config, kerko.DEFAULTS)
 
 # Update app configuration from environment variables.
-app.config.from_prefixed_env(prefix='MYAPP')
+app.config.from_prefixed_env(prefix="MYAPP")
 
 # Make changes to the Kerko configuration here, if desired.
-config_set(app.config, 'kerko.meta.title', 'My App')
+config_set(app.config, "kerko.meta.title", "My App")
 
 # Validate configuration and save its parsed version.
 parse_config(app.config)
 
 # Initialize the Composer object.
-app.config['kerko_composer'] = Composer(app.config)
+app.config["kerko_composer"] = Composer(app.config)
 
 # Make changes to the Kerko composer object here, if desired.
 
 babel = Babel(app)
 bootstrap = Bootstrap4(app)
 
-app.register_blueprint(kerko.make_blueprint(), url_prefix='/bibliography')
+app.register_blueprint(kerko.make_blueprint(), url_prefix="/bibliography")
